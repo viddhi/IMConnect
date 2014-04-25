6b514a0c6f40b9101b09d4968960194f2c5d01dc
viddhi <srividhya.gr@gmail.com>
2014-04-24 19:07:54 -0700

    Changed Icons, tried rounded rectangle background

--- gen/com/example/imconnect/R.java
+++ gen/com/example/imconnect/R.java
@@ -24,20 +24,22 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int imcheader=0x7f020001;
+        public static final int rounded_rectangle=0x7f020002;
     }
     public static final class id {
-        public static final int CmtCnt=0x7f080009;
-        public static final int DatePosted=0x7f080008;
-        public static final int Excerpt=0x7f080006;
+        public static final int CmtCnt=0x7f08000a;
+        public static final int DatePosted=0x7f080009;
+        public static final int Excerpt=0x7f080007;
         public static final int ExploreImc=0x7f080001;
-        public static final int FeaturedImage=0x7f080004;
-        public static final int HotOffTheOven=0x7f080000;
-        public static final int ID=0x7f080007;
-        public static final int LikeCnt=0x7f08000a;
-        public static final int SinglePostView=0x7f080002;
-        public static final int Title=0x7f080005;
-        public static final int action_settings=0x7f08000b;
-        public static final int textView1=0x7f080003;
+        public static final int FeaturedImage=0x7f080005;
+        public static final int HotOffTheOven=0x7f080002;
+        public static final int ID=0x7f080008;
+        public static final int LikeCnt=0x7f08000b;
+        public static final int SinglePostView=0x7f080003;
+        public static final int Title=0x7f080006;
+        public static final int TopTab=0x7f080000;
+        public static final int action_settings=0x7f08000c;
+        public static final int textView1=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
